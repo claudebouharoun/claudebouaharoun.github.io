2fe88d1038db5d35ca494ead587605c23ae36031
claudebouharoun <theclaudebouharoun@gmail.com>
2025-03-16 11:55:24 +0200

    Delete TEMPORARY WEBSITE directory

--- TEMPORARY WEBSITE/script.js
+++
@@ -1,6 +0,0 @@
-// Add tap/click interaction for mobile
-const container = document.querySelector('.container');
-
-container.addEventListener('click', function () {
-    container.classList.toggle('tap-active');
-});
